235aad0779d88b6a255a6590aa7f60c6662d4ed6
Hala-salah77 <hala.salah.ata@gmail.com>
2022-03-01 19:29:16 +0200

    adding some controller and edting in views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\GymController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +15,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(GymController::class)->group(function () {
+        Route::get('/gym/create', 'create');
+        Route::post('/gym/store', 'store');
+        Route::get('/gym/edit/{id}', 'edit');
+        Route::post('/gym/update/{id}', 'update');
+        Route::get('/gym/delete/{id}', 'delete');
+        Route::get('/gym/list', 'list');
+        Route::get('/gym/show/{id}', 'show');
 });
