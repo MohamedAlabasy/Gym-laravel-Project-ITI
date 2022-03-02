e0971833a6e2633d1fa4a286f69dc1f44c72c350
Hala-salah77 <hala.salah.ata@gmail.com>
2022-03-03 00:46:44 +0200

    editing in views

--- resources/views/layouts/user-layout.blade.php
+++ resources/views/layouts/user-layout.blade.php
@@ -454,7 +454,7 @@ body {
 
     <div id="sidebar-overlay"></div>
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <footer class="main-footer">
+    <footer class="main-footer" style="font-size:14px">
         <div class="float-right d-none d-sm-block">
             <b>Version</b> 3.2.0
         </div>
