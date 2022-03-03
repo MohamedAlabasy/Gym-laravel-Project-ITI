afc8264c15aaf3d3a0975600ce3de191b8928670
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-03-04 00:57:44 +0200

    test update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(AdminTableSeeder::class);
         // \App\Models\User::factory(10)->create();
+        $this->call(AdminTableSeeder::class);
     }
 }
