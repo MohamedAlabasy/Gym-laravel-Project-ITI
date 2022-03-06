304495a97aebecf15b489705fd06d3571dfca9f7
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-03-06 13:41:40 +0200

    start work on Revenue

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\City;
+use App\Models\Gym;
+use App\Models\Revenue;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    //
+    #=======================================================================================#
+    #			                               index                                       	#
+    #=======================================================================================#
+    public function index()
+    {
+        $cities = count(City::get('name'));
+        $citiesManagers =  count(User::role('cityManager')->get());
+        $gyms =  count(Gym::get('name'));
+        $gymsManagers =  count(User::role('gymManager')->get());
+        $coaches = count(User::role('coach')->get());
+        $users = count(User::role('user')->get());
+
+        $totalRevenue = (Revenue::sum('price')) / 100;
+        $revenueInDollars = number_format($totalRevenue, 2, ',', '.');
+
+        return view("welcome", [
+            'cities' => $cities,
+            'citiesManagers' => $citiesManagers,
+            'gyms' => $gyms,
+            'gymsManagers' => $gymsManagers,
+            'coaches' => $coaches,
+            'users' => $users,
+            'revenueInDollars' => $revenueInDollars,
+        ]);
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,58 +5,93 @@
         <div class="container-fluid pt-5">
             <!-- Small boxes (Stat box) -->
             <div class="row">
+                {{-- # ======================================= # Total Revenue # ======================================= # --}}
                 <div class="col-lg-3 col-6">
                     <!-- small box -->
                     <div class="small-box bg-info">
                         <div class="inner">
-                            <h3>150</h3>
-                            <p>Revenue</p>
+                            <h3>{{ $revenueInDollars }}</h3>
+                            <p>Total Revenue</p>
                         </div>
                         <div class="icon">
-                            <i class="fas fa-dollar-sign" style="font-size: 50px !important"></i>
+                            <i class="fas fa-dollar-sign text-white" style="font-size: 50px !important"></i>
                         </div>
                     </div>
                 </div>
-                <!-- ./col -->
+                {{-- # ======================================= # Cities # ======================================= # --}}
                 <div class="col-lg-3 col-6">
-                    <!-- small box -->
                     <div class="small-box bg-success">
                         <div class="inner">
-                            <h3>53<sup style="font-size: 20px"></sup></h3>
-                            <p>Coaches</p>
-                        </div>
-                        <div class="icon">
-                            <i class="fas fa-dumbbell" style="font-size: 50px !important"></i>
-                        </div>
-                    </div>
-                </div>
-                <!-- ./col -->
-                <div class="col-lg-3 col-6">
-                    <!-- small box -->
-                    <div class="small-box bg-warning">
-                        <div class="inner">
-                            <h3>44</h3>
-                            <p>Users</p>
-                        </div>
-                        <div class="icon">
-                            <i class="fas fa-users" style="font-size: 50px !important"></i>
-                        </div>
-                    </div>
-                </div>
-                <!-- ./col -->
-                <div class="col-lg-3 col-6">
-                    <!-- small box -->
-                    <div class="small-box bg-danger">
-                        <div class="inner">
-                            <h3>65</h3>
+                            <h3>{{ $cities }}</h3>
                             <p>Cities</p>
                         </div>
                         <div class="icon">
-                            <i class="fas fa-city" style="font-size: 50px !important"></i>
+                            <i class="fas fa-city text-white" style="font-size: 50px !important"></i>
                         </div>
                     </div>
                 </div>
-                <!-- ./col -->
+                {{-- # ======================================= # Cities Managers # ======================================= # --}}
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-secondary">
+                        <div class="inner">
+                            <h3>{{ $citiesManagers }}<sup style="font-size: 20px"></sup></h3>
+                            <p>Cities Managers</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-users text-white" style="font-size: 50px !important"></i>
+                        </div>
+                    </div>
+                </div>
+                {{-- # ======================================= # Gyms # ======================================= # --}}
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-danger">
+                        <div class="inner">
+                            <h3>{{ $gyms }}</h3>
+                            <p>Gyms</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-dumbbell text-white" style="font-size: 50px !important"></i>
+                        </div>
+                    </div>
+                </div>
+                {{-- # ======================================= # Gyms Managers # ======================================= # --}}
+                <div class="col-lg-3 col-6">
+                    <div class="small-box bg-warning">
+                        <div class="inner">
+                            <h3>{{ $gymsManagers }}<sup style="font-size: 20px"></sup></h3>
+                            <p>Gyms Managers</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-users text-dark" style="font-size: 50px !important"></i>
+                        </div>
+                    </div>
+                </div>
+                {{-- # ======================================= # Coaches # ======================================= # --}}
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-light">
+                        <div class="inner">
+                            <h3>{{ $coaches }}<sup style="font-size: 20px"></sup></h3>
+                            <p>Coaches</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-users text-dark" style="font-size: 50px !important"></i>
+                        </div>
+                    </div>
+                </div>
+                {{-- # ======================================= # Users # ======================================= # --}}
+                <div class="col-lg-3 col-6">
+                    <!-- small box -->
+                    <div class="small-box bg-dark">
+                        <div class="inner">
+                            <h3>{{ $users }}</h3>
+                            <p>Users</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fas fa-users text-white" style="font-size: 50px !important"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
             <!-- /.row -->
         </div>
