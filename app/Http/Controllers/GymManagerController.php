a7f16955f8369015fb96c7d7d1ec09b9973df783
MaiiEmad <eng.maii.emad@gmail.com>
2022-03-06 16:25:07 +0200

    gym manager create and list Done

--- app/Http/Controllers/GymManagerController.php
+++ app/Http/Controllers/GymManagerController.php
@@ -7,5 +7,19 @@ use Illuminate\Http\Request;
 
 class GymManagerController extends Controller
 {
-    //
+        //Create Function
+        public function create()
+        {
+            return view('gymManager.create');
+        }
+
+
+
+        //List Function
+        public function list(){
+            $usersFromDB=User::all();
+            // $usersFromDB =  User::role('cityManager')->get();
+            return view("gymManager.list",['users'=>$usersFromDB]);
+
+        }
 }
