d1936ac746a173d1faabb5db1c12a93ff2663419
MahmoudNehro <mahmoudnehro9000@gmail.com>
2022-03-07 19:16:01 +0200

    stripe beginning

--- config/services.php
+++ config/services.php
@@ -29,5 +29,8 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    'stripe' => [
+        'secret' => env('STRIPE_SECRET'),
+    ],
 
 ];
