beb3213e1429010cf37e61c634a9458b6e125efe
MahmoudNehro <mahmoudnehro9000@gmail.com>
2022-03-08 18:14:51 +0200

    modifiying view

--- resources/views/TrainingSessions/edit_training_session.blade.php
+++ resources/views/TrainingSessions/edit_training_session.blade.php
@@ -10,7 +10,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Edit Gym</h1>
+                    <h1>Edit Session</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
