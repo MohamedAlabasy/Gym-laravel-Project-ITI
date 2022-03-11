06365844bed5b6c97018867bf459bdc5c9d5d25c
gehad300 <gehad.ehab.mosaad@gmail.com>
2022-03-11 02:55:02 +0200

    attendence_history done Api

--- app/Http/Controllers/Api/SessionsController.php
+++ app/Http/Controllers/Api/SessionsController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Api;
-
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\TrainingSession;
@@ -26,9 +26,22 @@ class SessionsController extends Controller
         return ['total_sessions'=>Auth()->user()->total_sessions,
                 'remain_session'=>Auth()->user()->remain_session];
     }
-    public function attend_training_session(){
+    public function getSessionsForUser(Request $request){
+        $user=Auth()->user();
+        $history_attendances=Attendance::select(DB::raw('training_sessions.name as training_session_name,gyms.name as gym_name,Date(attendances.attendance_at
+        ) as attendance_date,Time(attendances.attendance_at) as attendance_time'))
+        ->join('users', 'users.id', '=', 'attendances.user_id')->join('training_sessions', 'training_sessions.id', '=', 'attendances.training_session_id')
+        ->join('gyms',"gyms.id",'=','users.gym_id')
+        ->where('attendances.user_id','=',$user->id)
+        ->where('users.id','=',$user->id)
 
+        ->get();
+        return response()->json(
+           $history_attendances
+        );
     }
 
 }
-
+// User will have endpoint to show his attendance history , and the
+// json object will be array that contains (training session name , gym
+// name , attendance date , attendance time
