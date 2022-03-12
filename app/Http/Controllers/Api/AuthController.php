d9dc95d220e25a3c16b1b4180ef89bf230b02345
Hala-salah77 <hala.salah.ata@gmail.com>
2022-03-12 16:12:12 +0200

    solve conflict

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -94,7 +94,6 @@ class AuthController extends Controller
             'password' => 'nullable|min:6',
             //we put here nullable cause user dosn't need to update his pasword every time
         ]);
-        if ($request->hasFile('profile_image')) {
             if ($request->hasFile('profile_image')) {
                 $image = $request->file('profile_image');
                 $name = time() . \Str::random(30) . '.' . $image->getClientOriginalExtension();
@@ -104,16 +103,8 @@ class AuthController extends Controller
                 if ($user->profile_image)
                     File::delete(public_path('imgs/' . $user->profile_image));
                 $user->profile_image = $imageName;
-
-                $image = $request->file('profile_image');
-                $name = time() . \Str::random(30) . '.' . $image->getClientOriginalExtension();
-                $destinationPath = public_path('/imgs');
-                $image->move($destinationPath, $name);
-                $imageName = 'imgs/' . $name;
-                if ($user->profile_image)
-                    File::delete(public_path('imgs/' . $user->profile_image));
-                $user->profile_image = $imageName;
             }
+
             $user->name = $request->name ? $request->name : $user->name;
             $user->email = $request->email ? $request->email : $user->email;
             $user->gender = $request->gender ? $request->gender : $user->gender;
@@ -126,4 +117,4 @@ class AuthController extends Controller
             ]);
         }
     }
-}
+
