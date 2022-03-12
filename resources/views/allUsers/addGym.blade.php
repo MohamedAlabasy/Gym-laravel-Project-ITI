6dc877815aa1dd446e0aa6f60d4e768e0298811d
Hala-salah77 <hala.salah.ata@gmail.com>
2022-03-12 14:17:19 +0200

    assign gym to  user

---
+++ resources/views/allUsers/addGym.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.user-layout')
+@section('content')
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper pb-4">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+                <div class="col-sm-6">
+                    <h4>Add Gym To User</h4>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Add Gym To User</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+    <!-- Main content -->
+    <section class="content">
+        <form action="{{url('/allUsers/submitGym',$user->id)}}" method="post"class="w-75 m-auto">
+            @csrf
+            <div class="row">
+                <div class="col-md-12">
+                    @if(session('status'))
+                    <h6 class="alart-success ">{{session('status')}}</h6>
+                @endif
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Adding Gym</h3>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                                    <i class="fas fa-minus"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label for="city">Gym</label>
+                                <select required class=" form-control" name="gym_id" id="city">
+                                    <optgroup label="Available City">
+                                        @foreach ($gyms as $gym)
+                                            <option value={{ $gym->id }}>{{ $gym->name }}</option>
+                                        @endforeach
+                                    </optgroup>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <a href="#" class="btn btn-secondary">Cancel</a>
+                    <input type="submit" value="Save Changes" class="btn btn-success float-right">
+                </div>
+            </div>
+        </form>
+
+    </section>
+</div>
+
+@endsection
