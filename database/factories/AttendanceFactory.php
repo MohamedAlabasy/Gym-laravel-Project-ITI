006d80a34ee78b21fbcd48d1c28d853216ea2f2a
MohamedAlabasy <Eng.mohamed.Alabasy@gmail.com>
2022-04-11 16:01:32 +0200

    add logo

--- database/factories/AttendanceFactory.php
+++ database/factories/AttendanceFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Attendance>
  */
